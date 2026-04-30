e2d927d8e6cf8544a83b6722956a6468b643ffa5
Rafi-Shariar <rafi.shariar619@gmail.com>
2026-05-01 00:09:26 +0600

    Ts functions & objects

--- m1 - Typescript/src/non-primitive.ts
+++ m1 - Typescript/src/non-primitive.ts
@@ -1,14 +1,15 @@
+//------- Array ----------
 let bazarList : string[] = ['eggs', 'milk', 'apple']
 
 let mixedArray : (string | number)[] = ['Rafi', 123]
 
-//tupple
+//--------- tupple --------
 let cordinates : [number, number] = [20,30]
 let couple : [string, number] = ['Rafi', 1054]
 let destination : [string, string, number] = ['Dhaka', 'Khulna', 3]
 
 
-//Object
+//--------- Object -----------------
 
 const user : {
     organization: "Programming Hero"; // value will work as a type : its called Literal Type
@@ -27,4 +28,24 @@ const user : {
     married: true
 }
 
-console.log(user);
+//-------- Function -----------
+
+function add ( num1:number, num2:number) : number {
+    return num1 + num2;
+}
+
+const add2 = (num1:number, num2:number) : number => num1 = num2;
+
+// function in object
+const poorUser = {
+    name: "mezba",
+    balance : 0,
+    addbalance( money : number) : number{
+       const newBalance =  this.balance + money
+       return newBalance
+    }
+}
+
+// funtion in loop
+const arr : number[] = [1,2,4];
+const sqrArr = arr.map((elem : number) : number => elem * elem)
