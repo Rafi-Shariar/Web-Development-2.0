e87d4671d67c64465037cf3a47d8c4e9357b3a94
Rafi-Shariar <rafi.shariar619@gmail.com>
2026-05-13 19:33:52 +0600

    connected postgres db

--- m7-express-postgres/src/server.ts
+++ m7-express-postgres/src/server.ts
@@ -1,9 +1,24 @@
-import express from "express"
-const app = express()
+import express, { type Application, type Request, type Response } from "express"
+import {Pool} from "pg";
+const app : Application = express()
 const port = 3000
+app.use(express.json()) // middleware
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+const pool = new Pool({
+    connectionString : "postgresql://neondb_owner:npg_QyYf8Gh4FHul@ep-ancient-bonus-aqzqi6jj-pooler.c-8.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require"
+})
+
+app.get('/', (req : Request, res : Response) => {
+  
+    res.status(200).json({
+        "message" : "Express Server",
+        "author" : "Rafi Shariar"
+    })
+})
+
+app.post('/', async (req : Request, res : Response)=>{
+    console.log(req.body);
+    
 })
 
 app.listen(port, () => {
