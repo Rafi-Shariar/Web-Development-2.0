bab0c29d59dbde2a6a5b1dfa6cba348d49304bae
Rafi-Shariar <rafi.shariar619@gmail.com>
2026-05-13 21:46:16 +0600

    created a table

--- m7-express-postgres/src/server.ts
+++ m7-express-postgres/src/server.ts
@@ -8,6 +8,32 @@ const pool = new Pool({
     connectionString : "postgresql://neondb_owner:npg_QyYf8Gh4FHul@ep-ancient-bonus-aqzqi6jj-pooler.c-8.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require"
 })
 
+const initDB = async () =>{
+
+    try {
+        await pool.query(`
+            CREATE TABLE IF NOT EXISTS users(
+                id SERIAL PRIMARY KEY,
+                name VARCHAR(30),
+                email VARCHAR(30) NOT NULL,
+                password VARCHAR(30) NOT NULL,
+                is_active BOOLEAN DEFAULT true,
+                age INT,
+                created_at TIMESTAMP DEFAULT NOW(),
+                updated_at TIMESTAMP DEFAULT NOW()
+            )
+            
+            `)
+        console.log('DB connected!');
+        
+    } catch (error) {
+        console.log(error);
+        
+    }
+
+}
+
+initDB()
 app.get('/', (req : Request, res : Response) => {
   
     res.status(200).json({
