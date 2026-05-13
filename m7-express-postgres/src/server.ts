19c1f11442bbe4f274034c04929e52b888c20b7b
Rafi-Shariar <rafi.shariar619@gmail.com>
2026-05-13 22:07:17 +0600

    inserting in DB

--- m7-express-postgres/src/server.ts
+++ m7-express-postgres/src/server.ts
@@ -1,21 +1,25 @@
-import express, { type Application, type Request, type Response } from "express"
-import {Pool} from "pg";
-const app : Application = express()
-const port = 3000
-app.use(express.json()) // middleware
+import express, {
+  type Application,
+  type Request,
+  type Response,
+} from "express";
+import { Pool } from "pg";
+const app: Application = express();
+const port = 3000;
+app.use(express.json()); // middleware
 
 const pool = new Pool({
-    connectionString : "postgresql://neondb_owner:npg_QyYf8Gh4FHul@ep-ancient-bonus-aqzqi6jj-pooler.c-8.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require"
-})
+  connectionString:
+    "postgresql://neondb_owner:npg_QyYf8Gh4FHul@ep-ancient-bonus-aqzqi6jj-pooler.c-8.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require",
+});
 
-const initDB = async () =>{
-
-    try {
-        await pool.query(`
+const initDB = async () => {
+  try {
+    await pool.query(`
             CREATE TABLE IF NOT EXISTS users(
                 id SERIAL PRIMARY KEY,
                 name VARCHAR(30),
-                email VARCHAR(30) NOT NULL,
+                email VARCHAR(30) UNIQUE NOT NULL,
                 password VARCHAR(30) NOT NULL,
                 is_active BOOLEAN DEFAULT true,
                 age INT,
@@ -23,30 +27,47 @@ const initDB = async () =>{
                 updated_at TIMESTAMP DEFAULT NOW()
             )
             
-            `)
-        console.log('DB connected!');
-        
-    } catch (error) {
-        console.log(error);
-        
-    }
+            `);
+    console.log("DB connected!");
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-}
+initDB();
+app.get("/", (req: Request, res: Response) => {
+  res.status(200).json({
+    message: "Express Server",
+    author: "Rafi Shariar",
+  });
+});
 
-initDB()
-app.get('/', (req : Request, res : Response) => {
-  
-    res.status(200).json({
-        "message" : "Express Server",
-        "author" : "Rafi Shariar"
-    })
-})
+// create a user & insert in table
+app.post("/", async (req: Request, res: Response) => {
+  const { name, email, password, age } = req.body;
 
-app.post('/', async (req : Request, res : Response)=>{
-    console.log(req.body);
-    
-})
+  try {
+    const result = await pool.query(
+      `
+            INSERT INTO users(name,email,password,age) VALUES ($1,$2,$3,$4)
+            RETURNING *
+        `,
+      [name, email, password, age],
+    );
+
+    console.log(result);
+    res.status(201).json({
+      message: "user created",
+      data: result.rows[0],
+    });
+  } catch (error: any) {
+    res.status(500).json({
+      message: error.message,
+      error: error,
+    });
+  }
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
