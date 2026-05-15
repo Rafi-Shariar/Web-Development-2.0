dc62c269368cd6da43edb8b834324e87ee97489d
Rafi-Shariar <rafi.shariar619@gmail.com>
2026-05-15 23:38:59 +0600

    CREATED PROFILE API

--- m8-authentication/src/app.ts
+++ m8-authentication/src/app.ts
@@ -5,6 +5,7 @@ import express, {
 } from "express";
 import { initDB, pool } from "./db";
 import { userRoute } from "./modules/user/user.route";
+import { profileRoute } from "./modules/profile/profile.route";
 const app: Application = express();
 
 app.use(express.json()); // middleware
@@ -19,13 +20,6 @@ app.get("/", (req: Request, res: Response) => {
 
 app.use('/api/users', userRoute)
 
-
-
-
-
-
-
-
-
+app.use('/api/profile', profileRoute)
 
 export default app;
