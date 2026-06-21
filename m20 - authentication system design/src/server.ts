a68273157f57dc764935e1745e7191b8ba40ed2e
Rafi-Shariar <rafi.shariar619@gmail.com>
2026-06-21 23:26:00 +0600

    server setup done

---
+++ m20 - authentication system design/src/server.ts
@@ -0,0 +1,23 @@
+import app from "./app";
+import config from "./config";
+import { prisma } from "./lib/prisma";
+const PORT = config.port;
+
+async function main(){
+    try {
+
+        await prisma.$connnect();
+        console.log("Connected to db successfully");
+        
+        app.listen(PORT, ()=>{
+            console.log(`server is running on port ${PORT}`);
+            
+        })
+    } catch (error) {
+        console.error("Error Starting the server", error);
+        await prisma.$disconnect();
+        process.exit(1);
+    }
+}
+
+main();
