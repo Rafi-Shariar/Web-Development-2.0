e655873fa1faf0ae857a896c3fd3ad6ab9101d6b
Rafi-Shariar <rafi.shariar619@gmail.com>
2026-07-03 23:58:09 +0600

    bug fixed on module 24

--- m20 - authentication system design/src/app.ts
+++ m20 - authentication system design/src/app.ts
@@ -18,8 +18,47 @@ app.use(cors({
     credentials:true
 }))
 
+const endpointSecret = config.webhook_secrete;
 
-app.use("/api/subscription/webhook",express.raw({type : 'application/json'}) )
+app.use("/api/subscription/webhook",express.raw({type : 'application/json'}), (request, response) => {
+  let event = request.body;
+//   console.log(event, "Stripe Request body");
+//   console.log(request.headers, "Stripe Request Headers");
+  
+
+  if (endpointSecret) {
+    
+    const signature = request.headers['stripe-signature']!;
+    try {
+      event = stripe.webhooks.constructEvent(
+        request.body,
+        signature, 
+        endpointSecret
+      );
+    } catch (err : any) {
+      console.log(`⚠️  Webhook signature verification failed.`, err.message);
+      return response.sendStatus(400);
+    }
+  }
+
+  console.log(event, "Event After Try Block");
+  
+
+  switch (event.type) {
+    case 'payment_intent.succeeded':
+      const paymentIntent = event.data.object;
+      console.log(`PaymentIntent for ${paymentIntent.amount} was successful!`);
+  
+      break;
+    case 'payment_method.attached':
+      const paymentMethod = event.data.object;
+    
+      break;
+    default:
+      
+      console.log(`Unhandled event type ${event.type}.`);
+  }
+})
 
 
 app.use(express.json())
