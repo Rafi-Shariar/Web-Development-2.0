ab16c7c137a69aac48db46973c40793aa0b03194
Rafi-Shariar <rafi.shariar619@gmail.com>
2026-07-24 20:49:45 +0600

    updated post

--- m27-nextjs-press-frontend/app/(dashboardGroup)/_actions/myPostActions.ts
+++ m27-nextjs-press-frontend/app/(dashboardGroup)/_actions/myPostActions.ts
@@ -102,3 +102,64 @@ export const createPost = async (prevState:PostState, formData : FormData ) =>{
     return {}
 
 }
+
+
+export const updatePost = async (postId : string ,prevState:PostState, formData : FormData ) =>{
+
+    const payload = {
+        title : formData.get("title") ?? "",
+        content : formData.get("content") ?? "",
+        thumbnail : formData.get("thumbnail") ?? "",
+        tags : ( formData.get("tags") as string).split(', ') ?? "",
+        isPremium : formData.get("isPremium") === "on"
+    }
+
+    
+    const cookieStore = await cookies();
+         const accessToken = cookieStore.get("accessToken")?.value;
+         if(!accessToken){
+             return {
+                 success : false,
+                 message : "user not logged in",
+     
+             }
+         }
+     
+         const res = await fetch(`${process.env.BACKEND_API_URL}/api/posts/${postId}`, {
+            method : "PATCH",
+             headers : {
+                "Content-Type": "application/json",
+                 Cookie: `accessToken=${accessToken}`
+             },
+             body : JSON.stringify(payload)
+         })
+     
+         const result = await res.json();
+
+        if(result.success){
+            revalidateTag("my-posts", {
+                expire : 0
+            })
+        }
+
+        if(result.success && result.data.isPremium){
+            revalidateTag("premium-posts", {
+                expire : 0
+            })
+        }
+        else{
+            revalidateTag("public-posts", {
+                expire : 0
+            })
+        }
+     
+     
+         return result
+
+    
+
+
+
+    return {}
+
+}
