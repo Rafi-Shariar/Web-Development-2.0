3ad1c49bbdb9ce088b303d1d97eb0238c0787104
Rafi-Shariar <rafi.shariar619@gmail.com>
2026-08-07 21:05:01 +0600

    module running

--- M34 - PH healthcare backend/src/app/module/auth/auth.controller.ts
+++ M34 - PH healthcare backend/src/app/module/auth/auth.controller.ts
@@ -113,9 +113,25 @@ const refreshToken = catchAsync(async (req: Request, res: Response) => {
 	});
 });
 
+const googleLogin = catchAsync(async (req: Request, res: Response) => {
+	
+	const payload = req.body;
+	const reuslt = await AuthService.googleLogin(payload)
+
+	sendResponse(res, {
+		statusCode: httpStatus.OK,
+		success: true,
+		message: "New tokens generated successfully",
+		data: {
+			
+		},
+	});
+});
+
 export const AuthController = {
 	registerPatient,
 	loginUser,
 	getMe,
 	refreshToken,
+	googleLogin
 };
